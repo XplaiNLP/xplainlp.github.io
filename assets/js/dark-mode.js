3813975e58c439f76430240aaa5d94a11a849d2d
Dr. Vera Schmitt <33310492+veraschmitt@users.noreply.github.com>
2025-01-31 10:53:28 +0100

    Update dark-mode.js

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById("dark-mode-toggle");
+    const currentTheme = localStorage.getItem("theme");
 
+    if (currentTheme) {
+        document.documentElement.setAttribute("data-theme", currentTheme);
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.documentElement.setAttribute("data-theme", "dark");
+    }
+
+    toggle.addEventListener("click", function () {
+        let newTheme = document.documentElement.getAttribute("data-theme") === "dark" ? "light" : "dark";
+        document.documentElement.setAttribute("data-theme", newTheme);
+        localStorage.setItem("theme", newTheme);
+    });
+});
